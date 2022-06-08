cf1efb96c614e9418c6921a86f13ff938e22744b
scrollcode4us <scrollcode.4.us@gmail.com>
2022-06-08 19:10:11 +0300

    git local init

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+
+	// Прокрутка 
+	$('.go-to').click(function (e) {
+		e.preventDefault();
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		$("body,html").animate({scrollTop: destination }, 1200);
+		});
+
+	
+
+});
+
